befcc4021ec598eb71554f5401243bba0d7a5ec9
kyoungmin <kstar701@gmail.com>
2023-03-29 18:28:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int a = System.in.read();
+        System.out.println(a);
+    }
+}
