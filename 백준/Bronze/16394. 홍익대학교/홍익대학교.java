1c4d3950e3952ff480e43bf7bc1fb349fa86e037
kyoungmin <kstar701@gmail.com>
2023-04-02 17:31:50 +0900

    [Bronze V] Title: 홍익대학교, Time: 204 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = 1946;
+        int x = sc.nextInt();
+        System.out.println(x-H);
+        sc.close();
+
+    }
+}
