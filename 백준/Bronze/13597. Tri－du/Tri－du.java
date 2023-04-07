1b617b55585366abdaa3856e955ebc9d38762952
kyoungmin <kstar701@gmail.com>
2023-04-07 23:30:09 +0900

    [Bronze IV] Title: Tri-du, Time: 212 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b)
+            System.out.println(a);
+        else if(a<b)
+            System.out.println(b);
+        else if(a==b)
+            System.out.println(a);
+        sc.close();
+    }
+}
