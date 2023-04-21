ab5a2444303595e688626303ea77987e46becf32
kyoungmin <kstar701@gmail.com>
2023-04-21 18:13:59 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toUpperCase();
+        
+        
+        return answer;
+    }
+}
