22a1441215672af71f9c8c9a100ff2e2a0a6ca16
kyoungmin <kstar701@gmail.com>
2023-04-22 16:39:45 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        answer = my_string.repeat(k);
+        return answer;
+        
+    }
+}
